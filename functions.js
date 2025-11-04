c029b4e8091160122db53f8802e0a45400ae9783
zubenathi721 <zubenathi.ncobo@gmail.com>
2025-11-04 08:08:50 +0000

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,15 @@
+function greet(name) {
+    return `Hello, ${name}!`
+}
+
+
+function add(a, b) {
+    return a + b
+}
+
+
+const salutations = greet("Anita")
+const sum = add(15, 20)
+
+console.log(salutations)
+console.log(sum)
